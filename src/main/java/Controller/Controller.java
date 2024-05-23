6bcb96fc7548d7190f8b113a59ff24efa5f65c16
Patricia <patricia.abadaranda@gmail.com>
2024-05-23 13:28:54 +0200

    Metodos processPostRequest y getBody añadidos.

--- src/main/java/Controller/Controller.java
+++ src/main/java/Controller/Controller.java
@@ -6,8 +6,7 @@ import Controller.Actions.*;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.*;
-import java.io.IOException;
-import java.io.PrintWriter;
+import java.io.*;
 
 @WebServlet(name = "Controller", urlPatterns = {"/Controller"})
 public class Controller extends HttpServlet {
@@ -97,10 +96,69 @@ public class Controller extends HttpServlet {
         processRequest(req, resp);
     }
 
+    private void processPostRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        response.setContentType("text/plain;charset=UTF-8");
+        response.setHeader("Access-Control-Allow-Origin", "*");
+        response.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+        response.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+        response.setHeader("Access-Control-Max-Age", "3600");
+        response.setContentType("text/plain;charset=UTF-8");
+
+        String strAction = request.getParameter("ACTION");
+        String[] arrayAction = new String[2];
+        if (!strAction.isEmpty()) {
+            arrayAction = strAction.split("\\.");
+
+            switch (arrayAction[0].toUpperCase()) {
+                case "PRODUCTOS": {
+                    response.getWriter().print(new ProductosAction().execute(request, response, arrayAction[1]));
+                    break;
+                }
+
+            }
+        }
+    }
+
+    public static String getBody(HttpServletRequest request) {
+
+        String body = null;
+        StringBuilder stringBuilder = new StringBuilder();
+        BufferedReader bufferedReader = null;
+
+        try {
+            InputStream inputStream = request.getInputStream();
+            if (inputStream != null) {
+                bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+                char[] charBuffer = new char[256];
+                int bytesRead = -1;
+                while ((bytesRead = bufferedReader.read(charBuffer)) > 0) {
+                    stringBuilder.append(charBuffer, 0, bytesRead);
+                }
+            } else {
+                stringBuilder.append("");
+            }
+        } catch (IOException ex) {
+            // throw ex; return "";
+        } finally {
+            if (bufferedReader != null) {
+                try {
+                    bufferedReader.close();
+                } catch (IOException ex) {
+
+                }
+            }
+        }
+
+        body = stringBuilder.toString();
+        return body;
+    }
+}
 
 // ENDPOINTS DE EJEMPLOS PARA QUE VEÁIS LA ESTRUCTURA
         //http://localhost:8080/untitled/Controller?ACTION=PELICULA.FIND_ALL
         // http://localhost:8080/API_JAVA_MYSQL/Controller?ACTION=PELICULA.FILTER&FILTRO=DRAMA
 //
 
-}
+
